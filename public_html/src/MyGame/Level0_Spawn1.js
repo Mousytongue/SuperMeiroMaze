1326452abc2001fe0960c31cd19d8fac5c61d027
Mousytongue <39573434+Mousytongue@users.noreply.github.com>
2019-03-07 23:35:06 -0800

    adjusted screen size

--- public_html/src/MyGame/Level0_Spawn1.js
+++ public_html/src/MyGame/Level0_Spawn1.js
@@ -11,7 +11,7 @@
 "use strict"
 
 Level0.prototype.SpawnWorld1 = function () {
-    this.Spawn2Init();
+    this.Spawn1Init();
 
     //1 = regular wall
     //r = right slanted wall
@@ -21,18 +21,18 @@ Level0.prototype.SpawnWorld1 = function () {
     //4 = large closing door   
     //5 = lazers! or falling rocks, TBD
     //6 = FallingRockSpawner
-    
-    var Row0 = "0000000000000000000000000000000000000000000000";  //Unseen, above camera
-    var Row1 = "6666666666666666666666666666666666666666666666";
-    var Row2 = "0000000000000000000000000000000000000000000000";
-    var Row3 = "0000000000000000000000000000000000000000000000";
-    var Row4 = "0000000000000000000000000000000000000000000000";
-    var Row5 = "0000000000000000000000000000000000000000000000";
-    var Row6 = "0000000000000000000000000000000000000000000000";
-    var Row7 = "0000000000000000000000000000000000000000000000";
-    var Row8 = "0000000000000000000000000000000000000000000000";
-    var Row9 = "11111111111________________1111111111111111111";
-    var Ro10 = "0000000000000000000000000000000000000000000000"; //Unseen, below camera
+                                                              //450 units 
+    var Row0 = "0000000111111111111111111111111111111111111111";  
+    var Row1 = "0000000000000000000000100000060000000000000000";
+    var Row2 = "0000000000000000000030010000006000000000000000";
+    var Row3 = "0000000000000000000003001000000600000000000000";
+    var Row4 = "0000000000000000000010300000000060000000000000";
+    var Row5 = "0000000000000000000001000000000006000000000000";
+    var Row6 = "0000000000000003000000100000000000600000000000";
+    var Row7 = "0000000000000030000000000000000000060000000000";
+    var Row8 = "0000000000000300000000000000000000000000000000";
+    var Row9 = "0000000000003000000000000000000000000000000000";
+    var Ro10 = "0000000111111111111111111111111111111111111111"; 
     this.mWorldArray[0] = Row0.split("");
     this.mWorldArray[1] = Row1.split("");
     this.mWorldArray[2] = Row2.split("");
@@ -71,8 +71,8 @@ Level0.prototype.SpawnWorld1 = function () {
 Level0.prototype.Spawn1Init = function () {
   this.mCamera = new Camera(
         vec2.fromValues(30, 50), // position of the camera
-        100,                     // width of camera
-        [0, 0, 800, 600]         // viewport (orgX, orgY, width, height)
+        200,                     // width of camera
+        [0, 0, 1600, 800]         // viewport (orgX, orgY, width, height)
     );
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
             // sets the background to gray
